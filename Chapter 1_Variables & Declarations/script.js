27c102ab6cf9fcdf84635509fd1fc31aa4ded325
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-08-03 22:50:01 +0530

    JS

---
+++ Chapter 1_Variables & Declarations/script.js
@@ -0,0 +1,15 @@
+// // 🧪 var, let, and const – Line-by-Line Comparison
+
+// var score = 10;
+// var score = 20; // OK
+
+// let age = 25;
+// age = 30; // ✅
+// let age = 40; // ❌ Error (same block)
+
+// const PI = 3.14;
+// PI = 3.14159; // ❌ Error
+
+// const student = { name: "Riya" };
+// student.name = "Priya"; // ✅ OK
+// student = {}; // ❌ Error
