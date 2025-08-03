0fabd68d0bb379c0e49b681a7f04af7201963ded
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-08-03 23:02:43 +0530

    JS

---
+++ Chapter 2_Data Types + Type System/script.js
@@ -0,0 +1,14 @@
+🔹 Primitive Data Types
+. String → Text
+"hello" , 'Sheryians'
+. Number → Any numeric value
+3 , -99 , 3.14
+. Boolean → True or false
+true , false
+. Undefined → Variable declared but not assigned
+let x; → x is undefined
+. Null → Intentional empty value
+let x = null;
+. Symbol → Unique identifier (rarely used)
+. BigInt → Very large integers
+123456789012345678901234567890n
