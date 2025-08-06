d636b640c48607559ebc4ac6fc3bea7b2fbcd791
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-08-06 11:56:22 +0530

    Update script.js

--- Chapter 1_Variables & Declarations/script.js
+++ Chapter 1_Variables & Declarations/script.js
@@ -13,3 +13,33 @@
 // const student = { name: "Riya" };
 // student.name = "Priya"; // ✅ OK
 // student = {}; // ❌ Error
+
+
+
+const name = "lokesh";
+const city = "Nagpur";
+
+let age = 22;
+
+let x = 5;
+let x = 10;
+
+
+console.log(count);
+var count = 42;
+
+
+const user = { name: "Lokesh" };
+user.age = 22;
+
+console.log(user); // { name: 'Lokesh', age: 22 }
+
+
+console.log(myVar); // ❌ ReferenceError
+let myVar = 10;
+
+
+const numbers = [1, 2, 3];
+numbers.push(4);
+
+console.log(numbers); // [1, 2, 3, 4]
